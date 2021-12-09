714ae1819a29a5bf71a991a4da75ad501b023d75
memiyuyu125gs <gjibxdfug0110@gmail.com>
2021-12-09 16:11:34 +0900

    Diary_Sakuramoto

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.SakuramotoDiary import SakuramotoDiary
 
 # 下のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [
+    DiarySample(), 
+    SakuramotoDiary(),
+    ]
 
 for d in diaries:
     print("------------------")
