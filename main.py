a2491e0cf930c7263486338e299e6475a1dfe6a8
Macha-Nai <95839522+Macha-Nai@users.noreply.github.com>
2021-12-09 16:18:31 +0900

    内木正紘日記追加！

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.NaikiDiary import NaikiDiary
 from diaries.TsutsuiDiary import TsutsuiDiary
 # 下のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(),
            TsutsuiDiary(),
+           NaikiDiary()
            ]
 
 for d in diaries:
